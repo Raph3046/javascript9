39a80a450077641c94304e582b32dd32cf59549c
Raph3046 <Raphael3046@gmail.com>
2022-09-25 16:21:17 +0100

    edit javascript

--- index5.js
+++ index5.js
@@ -3,7 +3,23 @@
 function randomFraction() {
 
 
-    return MMath.random();
+    return Math.random();
 }
 
 console.log(randomFraction()); 
+
+
+
+
+
+/* generate random whole number range */
+
+function randomRange(myMin, myMax) {
+
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+}
+
+let myRandom = randomRange(5,15);
+
+console.log(myRandom);
